816c6152c6d8bce5f532faae00f0690231213bc5
Thor Tepper-Garcia <ttepperg@gmail.com>
2026-09-18 16:18:19 +1000

    Make choice of server dependent on environ variable LLM_BACKEND

--- config.py
+++ config.py
@@ -8,17 +8,21 @@ TRACE_ON = True
 
 
 # SERVER DETAILS
+# Select via environmental variable (fallback: 'mock')
+# export LLM_BACKEND='mock' | 'gemini'
 
-# MOCK SERVER / LLM
-# LLM_CONFIG = {
-#     "base_url": "http://localhost:8000",
-#     "api_key": "fake-key", # a real key should not be exposed!
-#     "model": "mock-model",
-# }
-
-# Google / Gemini API
-LLM_CONFIG = {
-    "base_url": "https://generativelanguage.googleapis.com/v1beta/openai",
-    "api_key": os.environ["GEMINI_API_KEY"],
-    "model": "gemini-3.6-flash",
+LLM_BACKENDS = {
+    "mock": {
+        "base_url": "http://localhost:8000",
+        "api_key": "fake-key",
+        "model": "mock-model",
+    },
+    "gemini": {
+        "base_url": "https://generativelanguage.googleapis.com/v1beta/openai",
+        "api_key": os.environ.get("GEMINI_API_KEY"),
+        "model": "gemini-3.6-flash",
+    },
 }
+
+LLM_BACKEND = os.getenv("LLM_BACKEND", "mock")
+LLM_CONFIG = LLM_BACKENDS[LLM_BACKEND]

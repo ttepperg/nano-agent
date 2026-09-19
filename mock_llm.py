535bb1fa7a3e02da1882fc19322bf2bce9f83e69
Thor Tepper-Garcia <ttepperg@gmail.com>
2026-09-19 18:34:03 +1000

    Add mock capabilities

---
+++ mock_llm.py
@@ -0,0 +1,81 @@
+"""
+
+About
+    A mock Large Language Model (LLM) application programming interface (API)
+
+Called by
+
+    mock_server.py
+
+"""
+import json
+import random
+from utils import extract_keyval
+
+# response template
+response = {
+    "choices": [
+        { "message": {"content": None} }
+    ]
+}
+
+# Mock responses
+random_answer = \
+    ["Paris",
+    "Yes", "No", "Maybe",
+    "password", "secret"]
+
+# Mock memories
+random_memory = \
+    ["Bread", "Sun", "Walking", "Never", "Here", "Radical"]
+
+# Mock tasks
+task_choices = ["Add 1 and 5", "Upper case text", "Do something else"]
+
+def set_tools_openai_api(request, tool_index, args_dict, id):
+    """ Response schema: OpenAI-compatible Chat Completions """
+    select_tool = request["tools"][tool_index]
+    select_tool["function"]["arguments"] = json.dumps(args_dict)
+    select_tool["id"] = str(id)
+    return select_tool
+
+# ------ MOCK LLM ------
+def llm_api(request):
+    """
+        LLM emulator.
+        Uses an ad-hoc, deterministic logic to return responses.
+    """
+    select_tools = []
+    for roles in request["messages"]:
+        if roles["role"] == "user":
+            user_msg = roles["content"]
+    for umsg in user_msg.split('then'): # emulates multi-task request
+        if "add" in umsg.lower():
+            select_tool = \
+                set_tools_openai_api(request,0,{"a": 5, "b": 1},'1234')
+            select_tools.append(select_tool)
+            llm_response = {"tool_calls": select_tools}
+        elif "upper" in umsg.lower():
+            select_tool = \
+                set_tools_openai_api(request,1,{"text": "abcdef"},'5678')
+            select_tools.append(select_tool)
+            llm_response = {"tool_calls": select_tools}
+        elif any(s in umsg.lower() for s in ['age', 'name', 'birthplace']):
+            key, val = extract_keyval(umsg)
+            select_tool = \
+                set_tools_openai_api(request, 2, {"key": key, "value": val}, '8910')
+            select_tools.append(select_tool)
+            llm_response = {"tool_calls": select_tools}
+        elif 'do ' in umsg.lower():
+            task = random.choice(task_choices)
+            select_tool = \
+                set_tools_openai_api(request, 3, {"task": task}, '1112')
+            select_tools.append(select_tool)
+            llm_response = {"tool_calls": select_tools}
+        else:
+            word = random.choice(random_answer)
+            llm_response = {"content": word}
+
+    response["choices"][0]["message"] = {"role": "assistant", **llm_response}
+
+    return response
---
+++ mock_server.py
@@ -0,0 +1,72 @@
+"""
+
+About
+    A mock HTTP server that emulates an Large Language Model (LLM) application programming interface  (API)
+
+
+    Created with Sphynx
+
+Run
+    $> py312 mock_server.py
+
+    [and leave running...then run nano_agent.py]
+
+Needed by
+    nano_agent.py
+
+Calls
+    mock_llm.py
+
+"""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import mock_llm
+
+ # BEWARE: Use *only* for development:
+import importlib
+import utils
+import config
+
+class LLMHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        # Refresh modules content (in case it is modified while running)
+        # BEWARE: Use *only* for development
+        importlib.reload(mock_llm)
+        importlib.reload(utils)
+        importlib.reload(config)
+
+        # How many bytes are in the request body?
+        length = int(self.headers["Content-Length"])
+
+        # Read and decode the request body
+        body = self.rfile.read(length).decode("utf-8")
+
+        # Turn JSON text into a Python dictionary
+        request = json.loads(body)
+
+        # Can also simply print(body), but this is formatted:
+        utils.debug("\nReceived:\n", json.dumps(request, indent=2))
+
+        # Construct a *fake* LLM response
+        # In reality, the 'request' is POSTed to a real LLM API,
+        # which provides a 'response'
+        response = mock_llm.llm_api(request)
+
+        # Convert our Python dictionary to JSON text to bytes
+        response_json = json.dumps(response).encode("utf-8")
+
+        # Send the HTTP response
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", str(len(response_json)))
+        self.end_headers()
+
+        self.wfile.write(response_json)
+
+
+# Start the server
+server = HTTPServer(("localhost", 8000), LLMHandler)
+print("Mock LLM listening on http://localhost:8000")
+server.serve_forever() # keep running...
